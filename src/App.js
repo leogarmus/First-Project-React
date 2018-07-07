db33b52d604cebf502fb6dd8e5826c73ad5cd662
leogarmus <39808733+leogarmus@users.noreply.github.com>
2018-07-07 20:04:30 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -3,30 +3,57 @@ import logo from './logo.svg';
 import './App.css';
 
 import Grid from 'react-bootstrap/lib/Grid';
-import Row from 'react-bootstrap/lib/Row';
-import Col from 'react-bootstrap/lib/Col';
+import styled from 'styled-components';
 
+import Header from './components/Header'
+import Menu from './components/Menu'
+import Main from './components/Main'
+
+
+const url = process.env.PUBLIC_URL+'/img/bg.jpg'
+
+const HeaderWrapper = styled.header`
+  width: 100%;
+  height: 49px;
+  background-color: #242424;
+  opacity: 0.8;
+`
+const MenuWrapper = styled.div`
+  height: 89px;
+  padding-top: 20px;
+`
+
+const MainWrapper = styled.main`
+  height: 600px;
+  padding-top: 130px;
+  background: url(${url}) no-repeat;
+  background-size: cover;
+`
 class App extends Component {
   render() {
     return (
-       <div className="wrapper">
-        <SayFullName name="Petya" surname="Pupkin" link="vk.com" />
-        <SayFullName name="Ivan" surname="Pupkina" link="youtube.com" />
-        <SayFullName name="Sasha" surname="Durkpca" link="facebook.com" />
+      <div className="App">
+        <HeaderWrapper>
+          <Grid>
+            <Header />
+           </Grid> 
+        </HeaderWrapper>   
+        <MenuWrapper>
+          <Grid>
+            <Menu />
+          </Grid>
+        </MenuWrapper>
+        <MainWrapper>
+          <Grid>
+            <Main />
+          </Grid>
+        </MainWrapper>
+          
       </div>
     );
   }
 }
 
-
-function SayFullName(props){
-  return(
-    <div>
-    <h1> Моё имя {props.name}, фамилия - {props.surname}</h1>
-    <a href="{props.link}"> Ссылка нам мой профиль </a>
-    </div>
-    )
-}
 
 
 
